5d7f8d64b7a6ad78592570354d5b50f91146f8d8
ranjit <ranjitd-sys>
2026-02-06 12:16:34 +0530

    adidng telemetry

--- src/chunk.ts
+++ src/chunk.ts
@@ -1,14 +1,15 @@
-import { Effect, pipe, Chunk } from "effect";
+import { Effect, pipe, Chunk, Option } from "effect";
 import { extractText } from "unpdf";
 import { PdfEctraError } from "./error";
 import * as S from "@effect/schema/Schema";
-import { Option } from "effect";
+import { NodeSdk } from "@effect/opentelemetry"
+import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-base"
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http"
 
 export type Invoice = S.Schema.Type<typeof InvoiceSchema>;
 
 const InvoiceSchema = S.Struct({
   first: S.String,
-
   second: S.String.pipe(
     S.filter((s) => s.includes("ITEM DETAILS"), {
       message: () => "Your Invoice should contain ITEM DETAILS",
@@ -20,7 +21,6 @@ const InvoiceSchema = S.Struct({
     }),
   ),
 });
-
 
 const EffectExtractPdfData = Effect.gen(function* () {
   const file = Bun.file("../public/label.pdf");
@@ -34,19 +34,20 @@ const EffectExtractPdfData = Effect.gen(function* () {
     try: () => extractText(buffer),
     catch: (e) => new PdfEctraError(String(e)),
   });
+
   return text;
-});
+})
 
 export const effectValidateReturn = Effect.gen(function* () {
   const data = yield* EffectExtractPdfData;
   const rawData = data.text.join("\n");
 
   const lines = pipe(
-    rawData.split("\n"), 
+    rawData.split("\n"),
     Chunk.fromIterable,
-    Chunk.map((line) => line.replace(/\u0000/g, "").replace(/\r/g, "")),
+    Chunk.map((line: any) => line.replace(/\u0000/g, "").replace(/\r/g, "")),
   );
-  
+
   const itemDetailsIdx = Chunk.findFirstIndex(lines, (line) =>
     line.includes("ITEM DETAILS"),
   );
@@ -60,35 +61,42 @@ export const effectValidateReturn = Effect.gen(function* () {
   const indices = {
     items: Option.getOrElse(itemDetailsIdx, () => 0),
     billing: Option.getOrElse(billingIdx, () => Chunk.size(lines)),
-    totalIdx: Option.getOrElse(totalIdx, () => Chunk.size(lines))
+    totalIdx: Option.getOrElse(totalIdx, () => Chunk.size(lines)),
   };
 
+  const first = pipe(lines, Chunk.take(indices.items), Chunk.join("\n"));
 
-  
-  const first = pipe(
-    lines, 
-    Chunk.take(indices.items), 
-    Chunk.join("\n")
-  );
-  
   const second = pipe(
-    lines, 
+    lines,
     Chunk.drop(indices.items),
     Chunk.take(indices.billing - indices.items),
-    Chunk.join("\n")
+    Chunk.join("\n"),
   );
-  
-  const third = pipe(
-    lines, 
-    Chunk.drop(indices.totalIdx), 
-    Chunk.join("\n")
-  );
+
+  const third = pipe(lines, Chunk.drop(indices.totalIdx), Chunk.join("\n"));
 
   const result = { first, second, third };
-  
+
   return yield* S.decode(InvoiceSchema)(result);
-});
+})
 
 
-export const PDF = await Effect.runPromise(effectValidateReturn);
-console.log(PDF.second)
+
+
+const NodeSdkLive = NodeSdk.layer(() => ({
+  resource: { serviceName: "Pdf-to-csv" },
+  spanProcessor: new BatchSpanProcessor(new OTLPTraceExporter())
+}))
+const program = Effect.gen(function* (){
+  yield * EffectExtractPdfData;
+  yield* Effect.sleep(10);
+  yield* effectValidateReturn;
+})
+
+Effect.runPromise(
+  program.pipe(
+    Effect.provide(NodeSdkLive),
+    Effect.catchAllCause(Effect.logError)
+  )
+)
+export const PDF = await Effect.runPromise(effectValidateReturn)
